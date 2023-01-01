1dd804faeacb9f1d76bbe46438a07f4f11f3571b
shh <heun3316@naver.com>
2023-01-01 13:28:17 +0900

    study 230101

--- study2/src/App.js
+++ study2/src/App.js
@@ -1,29 +1,48 @@
-import React, {Component} from 'react';
+import React, {useState, Component} from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 
 
 function FuncComp(props){
+  var numberState = useState(props.initNumber);
+  var number = numberState[0]
+  var setNumber = numberState[1];
+
+  // var dateState = useState( (new Date()).toLocaleString() );
+  // var _date = dateState[0];
+  // var setDate = dateState[1];
+  var [_date, setDate] = useState( (new Date()).toLocaleString() ); //최신 useState방식
+
   return(
     <div className='container'>
       <h2>function style component</h2>
-      <p>Number : {props.initNumber}</p>
+      <p>Number : {number}</p>
+      <p>Date : {_date}</p>
+      <input type='button' value='random' onClick={() => {
+        setNumber( Math.floor(Math.random() * 11) ) }} />
+      <input type='button' value='date' onClick={() => {
+        setDate( (new Date()).toLocaleString() ) }} />
     </div>
   );
 }
 
 class ClassComp extends Component {
   state = {
-    number: this.props.initNumber
+    number: this.props.initNumber,
+    date: (new Date()).toLocaleString()
   }
   render(){
     return(
       <div className='container'>
         <h2>class style component</h2>
         <p>Number : {this.state.number}</p>
-        <input type='button' value='random' onClick={() => {
-          this.setState({number: Math.ceil(Math.random() * 10)});
-        }}></input>
+        <p>Date : {this.state.date}</p>
+        <input type='button' value='random' onClick={function(){
+          this.setState( {number: Math.ceil(Math.random() * 10)} );
+        }.bind(this)} />
+        <input type='button' value='date' onClick={function(){
+          this.setState( {date: (new Date()).toLocaleString()} );
+        }.bind(this)} />
       </div>
     );
   }
