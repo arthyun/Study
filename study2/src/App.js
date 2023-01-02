6cbb8424ac59faf04bd32ff94ca85675570839a8
shh <heun3316@naver.com>
2023-01-02 12:15:17 +0900

    study 230102

--- study2/src/App.js
+++ study2/src/App.js
@@ -1,8 +1,9 @@
-import React, {useState, Component} from 'react';
+import React, {useState, useEffect, Component} from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 
-
+var funcStyle = 'color:yellow';
+var funcId = 0;
 function FuncComp(props){
   var numberState = useState(props.initNumber);
   var number = numberState[0]
@@ -12,6 +13,50 @@ function FuncComp(props){
   // var _date = dateState[0];
   // var setDate = dateState[1];
   var [_date, setDate] = useState( (new Date()).toLocaleString() ); //최신 useState방식
+
+  //side effect(부수효과) +skipping effect추가
+  useEffect(() => {
+    console.log('%cfunc => useEffect (componentDidMount) ' + (funcId++), funcStyle);
+    document.title = number;
+    return () => {
+      console.log('%cfunc => useEffect return (componentWillUnMount) ' + (funcId++), funcStyle);
+    }
+  }, []);
+  useEffect(() => {
+    console.log('%cfunc => useEffect number (componentDidMount & componentDidUpdate) ' + (funcId++), funcStyle);
+    document.title = number;
+    return () => {
+      //console.log('%cfunc => useEffect number return (componentDidMount & componentDidUpdate) ' + (funcId++), funcStyle);
+    }
+  }, [number]);
+
+  useEffect(() => {
+    console.log('%cfunc => useEffect _date (componentDidMount & componentDidUpdate) ' + (funcId++), funcStyle);
+    document.title = _date;
+    return () => {
+      //console.log('%cfunc => useEffect _date return (componentDidMount & componentDidUpdate) ' + (funcId++), funcStyle);
+    }
+  }, [_date]);
+
+  //useEffect clean up
+  // function FriendStatus(props){
+  //   const [isOnline, setIsOnline] = useState(null);
+
+  //   useEffect(() => {
+  //     function handleStatusChange(status){
+  //       setIsOnline(status.isOnline);
+  //     }
+  //     ChatAPI.subscribeToFriendStatus(props.friend.id, handleStatusChange);
+  //     //effect 이후에 어떻게 정리할 것인지 표시.
+  //     return function cleanUp(){
+  //       ChatAPI.unsubsscribeFromFriendStatus(props.friend.id, handleStatusChange);
+  //     }
+  //   });
+  //   if(isOnline === null){
+  //     return 'Loading...';
+  //   }
+  //   return isOnline ? 'Online' : 'Offline';
+  // }
 
   return(
     <div className='container'>
@@ -26,12 +71,30 @@ function FuncComp(props){
   );
 }
 
+var classStyle = 'color:red';
 class ClassComp extends Component {
   state = {
     number: this.props.initNumber,
     date: (new Date()).toLocaleString()
   }
+  UNSAFE_componentWillMount(){
+    console.log('%cclass => componentWillMount', classStyle);
+  }
+  componentDidMount(){
+    console.log('%cclass => componentDidMount', classStyle);
+  }
+  shouldComponentUpdate(nextProps, nextState){
+    console.log('%cclass => shouldComponentUpdate', classStyle);
+    return true;
+  }
+  UNSAFE_componentWillUpdate(nextProps, nextState){
+    console.log('%cclass => componentWillUpdate', classStyle);
+  }
+  componentDidUpdate(nextProps, nextState){
+    console.log('%cclass => componentDidUpdate', classStyle);
+  }
   render(){
+    console.log('%cclass => render', classStyle);
     return(
       <div className='container'>
         <h2>class style component</h2>
