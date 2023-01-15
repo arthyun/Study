bf799f3e2fecaeefc7bffda3c7cf33ae32f7b726
shh <heun3316@naver.com>
2023-01-15 20:39:28 +0900

    study 230115_3

--- movie1/src/App.js
+++ movie1/src/App.js
@@ -4,6 +4,8 @@ import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Visual from './components/Visual';
 import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
 
 
 function App() {
@@ -14,8 +16,8 @@ function App() {
 
       <Routes>
         <Route path='/' element={<Home />} />
-        {/* <Route path='/about' element={} />
-        <Route path='/contact' element={} /> */}
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
       </Routes>
     </div>
   );
---
+++ movie1/src/components/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Contact = () => {
+    return (
+        <div className='contact'>
+            <h2>Contact 페이지입니다.</h2>
+        </div>
+    );
+};
+
+export default Contact;
