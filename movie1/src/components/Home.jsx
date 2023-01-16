3614e40ade0662ea14847a0d716abf23cdc024ea
shh <heun3316@naver.com>
2023-01-16 11:15:16 +0900

    study 230116_1

--- movie1/src/components/Home.jsx
+++ movie1/src/components/Home.jsx
@@ -2,9 +2,21 @@ import React from 'react';
 
 
 const Home = () => {
+    function test(){
+        console.log('text!');
+    }
     return (
         <div className='home'>
-            <h2>Home 페이지입니다.</h2>
+            <h2>영화 정보 공유하기</h2>
+            <p>공유하고 싶은 영화 정보를 입력해 주세요.</p>
+
+            <div className='formZone'>
+                <label htmlFor='title'>영화제목</label> &nbsp;
+                <input id='title' type='text' required /> <br/>
+                <label htmlFor='made'>제작사</label> &nbsp;
+                <input id='made' type='text' required />
+                <button onClick={test}>제출</button>
+            </div>
         </div>
     );
 };
