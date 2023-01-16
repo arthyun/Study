c4257aa755446ef50fb0ce67592f447f5910da78
shh <heun3316@naver.com>
2023-01-16 10:40:10 +0900

    study 230116

--- movie1/src/components/Visual.jsx
+++ movie1/src/components/Visual.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Data from '../visual.json';
 
 
 //로컬 json은 fetch와 같은 함수로 호출하는 것이 아닌 import로 해야한다.
 const Visual = () => {
+    useEffect(() => {
+        const vBtn = document.querySelectorAll('.vBtn');
+        let moveUl = document.querySelector('.visual ul');
+    
+        vBtn.forEach((el, i) => {
+            el.addEventListener('click', (e) => {
+                if(e.target.classList.contains('leftBtn')){
+                    moveUl.style.transform = 'translateX(0)';
+                } else if(e.target.classList.contains('rightBtn')) {
+                    moveUl.style.transform = 'translateX(-50%)';
+                }
+            })
+        });
+    }, []);
+    
+
     return (
         <div className='visual'>
             <ul>
@@ -14,6 +30,8 @@ const Visual = () => {
                 )
             }
             </ul>
+            <span className='vBtn leftBtn'>좌</span>
+            <span className='vBtn rightBtn'>우</span>
         </div>
     );
 };
