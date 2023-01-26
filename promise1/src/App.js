6752b4bfa2acfa2bf0fd993873d9d92bc0da062c
shh <heun3316@naver.com>
2023-01-26 10:31:19 +0900

    study 230126

---
+++ promise1/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Sass(Scss)부터 연결하자...</p>
+        <p>Promise, then, catch를 공부해보자...</p>
+        <p>async, await, try, catch를 공부해보자...</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
