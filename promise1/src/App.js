2f91fd6d810b344fa2237a3adb3ce0a67cb62dea
shh <heun3316@naver.com>
2023-01-26 13:05:27 +0900

    study 230126_1

--- promise1/src/App.js
+++ promise1/src/App.js
@@ -2,13 +2,32 @@ import logo from './logo.svg';
 import './App.scss';
 
 function App() {
+  //첫번째 Promise 생성자 작성
+  //Promise를 생성하면 기본적으로 pending(대기)상태이다. -> fulfilled(이행) -> rejected(거부)
+  const promise1 = new Promise((resolve, reject) => {
+    const a = 1 + 1;
+    if(a === 2){
+      resolve('success');
+    } else {
+      reject('failed');
+    }
+  });
+  //결과 출력해보기
+  promise1.then((msg) => {
+    console.log('결과: ' + msg);
+  }).catch((error) => {
+    console.log('결과: ' + error);
+  })
+
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Sass(Scss)부터 연결하자...</p>
-        <p>Promise, then, catch를 공부해보자...</p>
-        <p>async, await, try, catch를 공부해보자...</p>
+        <p><strong>Sass(Scss)</strong> 먼저 연결하자...</p>
+        <p><strong>Promise, then, catch</strong>를 공부해보자...</p>
+        <p><strong>async, await, try, catch</strong>를 공부해보자...</p>
       </header>
     </div>
   );
