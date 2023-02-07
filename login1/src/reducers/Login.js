b67abf88bc217018aa54ee5e5eb57471bd0bd621
shh <heun3316@naver.com>
2023-02-07 19:05:13 +0900

    study 230207

---
+++ login1/src/reducers/Login.js
@@ -0,0 +1,14 @@
+let initialState = {
+    user : {
+        id: "heun3316",
+        pass: "hyun7212"
+    }
+}
+const Login = (state = initialState, action) => {
+    switch(action.type){
+        default:
+            return state;
+    }
+};
+
+export default Login;
---
+++ login1/src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from "redux";
+import Login from './Login';
+
+const rootReducer = combineReducers({
+    Login,
+});
+
+export default rootReducer;
