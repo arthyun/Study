7ac0a1c39e1a5bdf1d6623281ce44005e070da83
shh <heun3316@naver.com>
2023-02-09 14:27:52 +0900

    study 230209_1

--- styledcompo1/src/App.tsx
+++ styledcompo1/src/App.tsx
@@ -7,7 +7,8 @@ function App() {
     <div className="App">
       <Button color='green' background='pink'>취준이란 힘든 삶인거 같다.</Button><br/>
       <Button color='blue' background='aqua'>방향을 바꾸는게 좋을까.</Button><br/>
-      <Button color='black' background='yellow'>자괴감에 빠진다.</Button>
+      <Button color='black' background='yellow'>자괴감에 빠진다.</Button><br/>
+      <Button primary='primary'>Primary Button</Button>
     </div>
   );
 }
--- styledcompo1/src/components/Button.tsx
+++ styledcompo1/src/components/Button.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import styled, { css } from 'styled-components';
 
+//props를 대입할때는 함수로서 가져와야함
 const StyledButton = styled.button<Props>`
     padding: 10px 20px;
     color: ${(props) => props.color || 'gray'};
     background: ${(props) => props.background || 'white'};
-`; //props를 대입할때는 함수로서 가져와야함
+
+    ${(props) =>
+        props.primary &&
+        css`
+        color: white;
+        background: navy;
+        border-color: navy;
+    `}
+`; 
 
 interface Props {
     [key : string]: string;
