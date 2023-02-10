fef9e03cc8befd7067af0a59cecc4229dc2a592c
shh <heun3316@naver.com>
2023-02-10 18:26:35 +0900

    study 230210_1

--- diary1/src/App.js
+++ diary1/src/App.js
@@ -1,23 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react'
+import styled from 'styled-components';
+
+const Inputs = styled.input.attrs({ type: 'text', required: true })`
+  background: #61dafb;
+  color: #333;
+  font-size: 18px;
+  outline: 0;
+  border: 0;
+  display: block;
+  box-sizing: border-box;
+  width: 80%;
+  height: 40px;
+  margin: 0 auto;
+  padding-left: 10px;
+`;
+
+const SubmitBtn = styled.button.attrs({ type: 'button' })`
+  background: #eee;
+  font-size: 16px;
+  font-weight: bold;
+  display: block;
+  box-sizing: border-box;
+  width: 81%;
+  height: 40px;
+  margin: 0 auto;
+`;
 
 function App() {
+  const [text, setText] = useState('');
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+
+      <div className='keyZone'>
+        <Inputs value={text} placeholder='작성하세요.' 
+        onChange={(e) => {
+          setText(e.target.value);
+          console.log(text);
+        }} />
+        <SubmitBtn onClick={() => {
+          if(window.confirm('등록하시겠습니까?')){
+            alert('등록이 완료되었습니다.');
+          } else {
+            alert('다시 확인해 주세요.');
+          }
+        }}>등록</SubmitBtn>
+      </div>
     </div>
   );
 }
