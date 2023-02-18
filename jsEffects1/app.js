3baee20bd7a0d562df8647f8a8b8db56e295b2d9
shh <heun3316@naver.com>
2023-02-18 15:19:34 +0900

    study 230218_1

--- jsEffects1/app.js
+++ jsEffects1/app.js
@@ -1,3 +1,4 @@
+//slider
 const moveBox = document.querySelector('.slider_main');
 const list = document.querySelectorAll('.slider_main div');
 const totalWidth = list[0].clientWidth * list.length;
@@ -27,3 +28,37 @@ btns.forEach(el => {
         }
     })
 })
+
+//tab
+const tabBtns = document.querySelectorAll('.tabBtns a');
+const tabPrintMain = document.querySelector('.tabPrint');
+const tabPrint = document.querySelectorAll('.tabPrint p');
+
+tabPrint[0].style.position = 'relative';
+tabPrint[0].style.left = 0;
+
+tabBtns.forEach((el, i) => {
+    el.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        tabPrint.forEach(le => {
+            le.style.position = 'absolute';
+            le.style.left = '-100%';
+        });
+
+        tabPrint[i].style.position = 'relative';
+        tabPrint[i].style.left = 0;
+        
+        // if(el.classList.contains('1')){
+        //     tabPrint[0].style.display = 'block';
+        // } else if(el.classList.contains('2')){
+        //     tabPrint[1].style.display = 'block';
+        // } else if(el.classList.contains('3')){
+        //     tabPrint[2].style.display = 'block';
+        // } else if(el.classList.contains('4')){
+        //     tabPrint[3].style.display = 'block';
+        // }
+
+
+    })
+});
