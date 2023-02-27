45c03c1aaec49ccc1e7d64d05926942a5ff22835
shh <heun3316@naver.com>
2023-02-27 11:14:10 +0900

    study 230227

--- next1/pages/navbar.js
+++ next1/pages/navbar.js
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import styles from "../styles/navbar.module.css";
 
 
 const Navbar = () => {
@@ -7,7 +8,7 @@ const Navbar = () => {
     console.log(router);
     //react router와는 다르게 Link태그는 경로만 지정가능하며 추가 속성을 이용하려면 Link안에 a태그를 지정해야한다.
     return (
-        <>
+        <nav>
         {/* Link 구버전 */}
         {/* <Link href='/'>
             <a style={{color: 'red'}}>Home</a>
@@ -17,9 +18,13 @@ const Navbar = () => {
         </Link> */}
 
         {/* Link 신버전 */}
-        <Link href='/' style={{color: router.pathname === "/" ? "yellow" : "#fff"}}>Home</Link>
-        <Link href='/about' style={{color: router.pathname === "/about" ? "yellow" : "#fff"}}>About</Link>
-        </>
+        {/* <Link href='/' style={{color: router.pathname === "/" ? "yellow" : "#fff"}}>Home</Link>
+        <Link href='/about' style={{color: router.pathname === "/about" ? "yellow" : "#fff"}}>About</Link> */}
+
+        {/* 클래스 2개 이상 추가 시 콤마 사용하지 않기 */}
+        <Link href='/' className={`${styles.link} ${router.pathname === "/" ? styles.active : ""}`}>Home</Link>
+        <Link href='/about' className={router.pathname === "/about" ? styles.active : ""}>About</Link>
+        </nav>
     )
 }
 export default Navbar;
