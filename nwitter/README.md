feb79495e4f2a4e4bb651be90d43f5a4881a54fa
shh <heun3316@naver.com>
2023-03-07 14:11:36 +0900

    nwitter v02 230307

--- nwitter/README.md
+++ nwitter/README.md
@@ -9,4 +9,12 @@
  - import "firebase/compat/firestore";
  - import "firebase/compat/storage";
  
-3. npm install react-router-dom@5.3.0 -> 구버전이 좋다면!
+4. Firebase SDK 추가하기
+ - src에 firebase.js 생성하기
+ - Firebase 페이지에서 웹 추가 후 SDK값 받아오기(config/initializeApp)
+ - firebase.js에 해당 내용 추가해주기
+ - 가장 상단에 firebase/app을 import 해주기(작성된 firebase.js 파일 확인)
+
+5. index.js에서 firebase.js import해주기
+
+추가) npm install react-router-dom@5.3.0 -> 구버전이 좋다면!
---
+++ nwitter/src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/compat/app';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDzBvgfRV7KVifngKcZiugC6jeUxuBC2YI",
+    authDomain: "nwitter-709ed.firebaseapp.com",
+    projectId: "nwitter-709ed",
+    storageBucket: "nwitter-709ed.appspot.com",
+    messagingSenderId: "978091235519",
+    appId: "1:978091235519:web:93da545606922dcc3fce0a"
+};
+// Initialize Firebase
+export default firebase.initializeApp(firebaseConfig);
