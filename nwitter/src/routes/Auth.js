6439ecfba439af80543d19c4539a3dbeb1ce2569
shh <heun3316@naver.com>
2023-03-09 20:24:58 +0900

    nwitter v08 230309

--- nwitter/src/routes/Auth.js
+++ nwitter/src/routes/Auth.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Auth = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const onChange = (e) => {
+        const tg = e.target.name;
+        if(tg === "email"){
+            setEmail(e.target.value);
+        } else if(tg === "password"){
+            setPassword(e.target.value);
+        }
+    }
+    const onSubmit = (e) => {
+        e.preventDefault();
+        
+    }
 
     return(
-        <div>Auth</div>
+        <div>
+            <form onSubmit={onSubmit}>
+                <input name='email' type='text' placeholder='Email' required 
+                value={email} onChange={onChange} />
+                <input name='password' type='password' placeholder='Password' 
+                required value={password} onChange={onChange} />
+                <input type='submit' value='Log In' />
+            </form>
+
+            <div>
+                <button>continue with Google</button>
+                <button>continue with Github</button>
+            </div>
+        </div>
     );
 }
 
