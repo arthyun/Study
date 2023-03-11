19fdf17ca4bba294d97af40bac4f25603a4a2a89
shh <heun3316@naver.com>
2023-03-11 14:12:04 +0900

    nwitter v10 230311

--- nwitter/src/components/App.js
+++ nwitter/src/components/App.js
@@ -1,17 +1,31 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import AppRouter from './Router';
 import { BrowserRouter } from 'react-router-dom';
 import { auth } from '../firebase.js';
 
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(auth.currentUser);
-  console.log(auth.currentUser);
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  
+  //onAuthStateChanged 함수로 auth의 상태를 체크할 수 있다.
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if(user){
+        //user가 있을때
+        setIsLoggedIn(true);
+      } else {
+        //user가 없을때
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, []);
 
   return (
     <BrowserRouter>
       <div className="App">
-          <AppRouter isLoggedIn={isLoggedIn} />
+          {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "Initializing..."}
           <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
       </div>
     </BrowserRouter>
--- nwitter/src/routes/Auth.js
+++ nwitter/src/routes/Auth.js
@@ -1,9 +1,13 @@
 import React, { useState } from 'react';
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
 
 const Auth = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [newAccount, setNewAccount] = useState(true);
+    const [error, setError] = useState("");
 
+    //구 방식
     // const onChange = (e) => {
     //     const tg = e.target.name;
     //     if(tg === "email"){
@@ -21,9 +25,29 @@ const Auth = () => {
             setPassword(value);
         }
     }
-    const onSubmit = (e) => {
+    const onSubmit = async (e) => {
         e.preventDefault();
-        
+        //auth를 불러온 후 newAccount 여부에 따라 기능 변경
+        const auth = getAuth();
+        let data;
+        try {
+            if(newAccount){
+                //create account
+                data = await createUserWithEmailAndPassword(auth, email, password);
+                console.log('계정이 생성 되었습니다.');
+            } else {
+                //log in
+                data = await signInWithEmailAndPassword(auth, email, password);
+                console.log('로그인 되었습니다.');
+            }
+        } catch(err) {
+            setError(err.message);
+        }
+    }
+    //Log In 버튼 수정
+    const toggleAccount = () => {
+        //클릭 시 newAccount 값이 반대되게
+        setNewAccount(prev => !prev);
     }
 
     return(
@@ -33,12 +57,17 @@ const Auth = () => {
                 value={email} onChange={onChange} />
                 <input name='password' type='password' placeholder='Password' 
                 required value={password} onChange={onChange} />
-                <input type='submit' value='Log In' />
+                <input type='submit' value={ newAccount ? "Create Account" : "Sign In" } />
             </form>
 
+            <span onClick={toggleAccount}>
+                { newAccount ? "Sign In" : "Create Account" }
+            </span>
+
             <div>
-                <button>continue with Google</button>
-                <button>continue with Github</button>
+                <button name='google'>continue with Google</button>
+                <button name='github'>continue with Github</button>
+                {error}
             </div>
         </div>
     );
