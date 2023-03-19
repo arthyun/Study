ce43f0ac0eb2632fb346339a31756c5812948b5d
shh <heun3316@naver.com>
2023-03-19 15:14:55 +0900

    study useRef 230319_1

--- study4/src/components/Nav.js
+++ study4/src/components/Nav.js
@@ -1,4 +1,9 @@
 import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+
+const Nav_A = styled.nav`
+    background: red;
+`;
 
 const Nav = (props) => {
     //출력할 태그 만들기
@@ -14,12 +19,30 @@ const Nav = (props) => {
         </li>
     );
 
+    
+
     return (
-        <nav>
+        <Nav_A>
             <ul>
                 {lists}
             </ul>
-        </nav>
+
+            <style jsx='true'>
+                {`
+                    nav ul li {
+                        padding: 5px 0;
+                        padding-left: 15px;
+                    }
+                    nav ul li a {
+                        text-decoration: none;
+                        color: #fff;
+                        font-weight: bold;
+                    }
+                `}
+            </style>
+        </Nav_A>
+
+        
     );
 }
 
