4b69bfb0f6be71a4981fede005a3b7fd0a3c9534
shh <heun3316@naver.com>
2023-06-12 09:20:11 +0900

    Add Vanilla-Redux Project 230612

--- next2/app/page.js
+++ next2/app/page.js
@@ -1,10 +1,12 @@
 import Image from 'next/image';
 import styles from './page.module.css';
+import Carousel from './carousel';
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
+      <Carousel />
         <p>
           Get started by editing&nbsp;
           <code className={styles.code}>app/page.js</code>
