d736a65fca1a04994329e3016b9bf6912b1e518a
shh <heun3316@naver.com>
2023-06-14 09:20:38 +0900

    study vanilla-redux 230614

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -1,11 +1,12 @@
 import { createStore } from 'redux';
 
-const plus = document.getElementById('add');
+const add = document.getElementById('add');
 const minus = document.getElementById('minus');
 const number = document.querySelector('span');
 
+//Reducer 정의
 const countModifier = (state = 0, action) => {
-  //action 타입 정의
+  //action은 항상 객체로서 전달되어야 함
   switch(action.type){
     case "ADD":
       return state + 1;
@@ -15,30 +16,26 @@ const countModifier = (state = 0, action) => {
       return state;
   };
 };
+
+//Store 정의
 const countStore = createStore(countModifier);
 
-// countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "MINUS" });
+const onChange = () => {
+  // console.log('there was change on the store');
+  number.innerText = countStore.getState();
+}
 
-console.log(countStore.getState());
+const handleAdd = () => {
+  countStore.dispatch({ type: "ADD" });
+  // console.log(countStore.getState());
+};
+const handleMinus = () => {
+  countStore.dispatch({ type: "MINUS" });
+  // console.log(countStore.getState());
+};
 
+add.addEventListener('click', handleAdd);
+minus.addEventListener('click', handleMinus);
 
-//바닐라만 사용해서 변수를 조작했다면...
-// let count = 0;
-// number.innerText = count;
-
-// const updateText = () => {
-//   number.innerText = count;
-// };
-
-// const handleAdd = () => {
-//   count = count + 1;
-//   updateText();
-// };
-// const handleMinus = () => {
-//   count = count - 1;
-//   updateText();
-// };
-
-// plus.addEventListener('click', handleAdd);
-// minus.addEventListener('click', handleMinus);
+//store 변화를 감지하면 콜백함수 실행
+countStore.subscribe(onChange);
