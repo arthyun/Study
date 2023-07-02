2facda711521080d7e8a51db35767166ef1bf03b
shh <heun3316@naver.com>
2023-07-02 12:07:08 +0900

    study 230702_1

--- next3/src/app/page.tsx
+++ next3/src/app/page.tsx
@@ -1,20 +1,36 @@
 import Image from 'next/image';
 import styles from '../styles/page.module.css';
+import Link from 'next/link';
 
 
 export default function Home() {
 
+
   return (
-    <main className={styles.main}>
-      <Image
-        src="/next.svg"
-        alt="Next Logo"
-        className={styles.logo}
-        width={300}
-        height={48}
-        priority
-      />
-      
-    </main>
+    <div className='wrap'>
+      <header>
+        <ul>
+            <li>
+                <Link href='/'>Home</Link>
+            </li>
+            <li>
+                <Link href='/sub1'>Sub1</Link>
+            </li>
+            <li>
+                <Link href='/sub2'>Sub2</Link>
+            </li>
+        </ul>
+      </header>
+      <main className={styles.main}>
+        <Image
+          src="/next.svg"
+          alt="Next Logo"
+          className={styles.logo}
+          width={300}
+          height={48}
+          priority
+        />
+      </main>
+    </div>
   )
 }
