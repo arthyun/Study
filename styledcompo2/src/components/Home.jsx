1c942f39959db412455fb59d561cef82406b716e
arthyun <heun3316@naver.com>
2023-08-07 19:30:30 +0900

    mod styledcompo2 230807_1

--- styledcompo2/src/components/Home.jsx
+++ styledcompo2/src/components/Home.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import styled from 'styled-components';
 
 const HomeWrap = styled.div`
@@ -5,11 +6,29 @@ const HomeWrap = styled.div`
     width: 300px;
     height: 300px;
     border: 2px solid #fff;
-`
+`;
+const FirstInput = styled.input.attrs({required: true})`
+    width: 100%;
+    padding: .75rem;
+    box-sizing: border-box;
+    background: pink;
+    font-size: 20px;
+    color: #2f00ff;
+`;
 
 const Home = () => {
+    const [text, setText] = useState('');
+
+    const onChange = (e) => {
+        const { value } = e.target;
+        setText(value);
+    };
+
     return (
-        <HomeWrap>Home</HomeWrap>
+        <HomeWrap>
+            Home
+            <FirstInput type='text' value={text} onChange={onChange} />
+        </HomeWrap>
     )
 }
 
