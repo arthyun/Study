6f1e9ecac7104733027ee813eb33b74c5c9430cb
arthyun <heun3316@naver.com>
2023-08-28 19:43:59 +0900

    study Array.from() 230828

--- VITE/client/src/components/Contact.jsx
+++ VITE/client/src/components/Contact.jsx
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 const Contact = () => {
+    const count = useRef(10);
+    const increCnt = () => {
+        count.current++;
+        // console.log(count.current);
+    }
+
+    const [newArr, setNewArr] = useState([]);
+    const changeArr = () => {
+        const editArr = Array.from({length: count.current}, (_, index) => {
+            let id = index + 1;
+            let down = _;
+            return {id, down};
+        });
+        // return setNewArr(prev => [...prev, ...editArr]);
+        return setNewArr(prev => [...editArr]);
+    }
+    // console.log(newArr);
+
+    useEffect(() => {
+        changeArr();
+    }, [])
 
     return (
-        <h1 style={{ padding: '1rem 2rem', boxSizing: 'border-box' }}>Contact Page.</h1>
+        <div style={{ padding: '1rem 2rem', boxSizing: 'border-box' }}>
+            <h1>Contact Page.</h1>
+            <h3>{count.current}</h3>
+            <button type='button' onClick={increCnt}>Increase</button>
+            <button type='button' onClick={changeArr}>배열생성</button>
+
+            {
+                newArr.map((item, i) => {
+                    return <p key={i}>당신의 순서는 {item.id}번 입니다.</p>
+                })
+            }
+        </div>
     )
 }
 
