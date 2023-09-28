801a571b009d568515d7d908dd29c670f30a98c0
arthyun <heun3316@naver.com>
2023-09-28 19:27:11 +0900

    study JWT v03

--- typescript4/server.js
+++ typescript4/server.js
@@ -1,12 +1,12 @@
 // 서버 세팅
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const jwt = require('./jwt-utils/jwt-util');
-const redisClient = require('./jwt-utils/redis');
-const authJWT = require('./jwt-utils/authJWT');
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const jwt = require("./jwt-utils/jwt-util");
+const redisClient = require("./jwt-utils/redis");
+const authJWT = require("./jwt-utils/authJWT");
 // const cors = require("cors"); // proxy 설정을 했기에 불필요
 // const db = require('./database');
 
@@ -30,17 +30,17 @@ app.use(cookieParser());
 // });
 
 // 로그인 API
-app.post('/api/login', async (req, res) => {
+app.post("/api/login", async (req, res) => {
   const { id, password } = req.body;
   const userId = id;
   const userPass = password;
   const user = {
     id: userId,
-    password: userPass
+    password: userPass,
   };
-  if (user.id === 'son' && user.password === '3316') {
+  if (user.id === "son" && user.password === "3316") {
     /* DB 쿼리문 */
-    // db.query(`select * from member where userName=? and pass=?`, [userName, pass], function (error, results, fields) {
+    // db.query(`select * from member where userName=? and pass=?`, [userId, userPass], function (error, results, fields) {
     //   if (error) {
     //     console.error(error);
     //   }
@@ -54,60 +54,60 @@ app.post('/api/login', async (req, res) => {
     // access token과 refresh token을 발급합니다.
     const accessToken = jwt.sign(user);
     const refreshToken = jwt.refresh();
-    console.log('엑세스토큰: ', accessToken);
-    console.log('리프레시토큰: ', refreshToken);
+    console.log("엑세스토큰: ", accessToken);
+    console.log("리프레시토큰: ", refreshToken);
 
     // 발급한 refresh token을 redis에 key를 user의 id로 하여 저장합니다.
     await redisClient.set(user.id, refreshToken);
     return res.status(200).json({
       // client에게 토큰 모두를 반환합니다.
-      status: 'success',
+      status: "success",
       data: {
         accessToken,
-        refreshToken
-      }
+        refreshToken,
+      },
     });
   } else {
-    console.log('로그인에 실패했습니다.');
-    return res.status(401).json({ status: 'failed', message: '로그인에 실패했습니다.' });
+    console.log("로그인에 실패했습니다.");
+    return res.status(401).json({ status: "failed", message: "로그인에 실패했습니다." });
   }
 });
 
 // 토큰 상태 확인
-app.get('/api/tokenVerify', authJWT, (req, res) => {
+app.get("/api/tokenVerify", authJWT, (req, res) => {
   // console.log(req.id);
   // console.log(req.password);
 
   if (req.id && req.password) {
     // 임시
-    res.json('요청 값이 있습니다.');
+    res.json("요청 값이 있습니다.");
   }
 });
 
 // user list API
-app.get('/api/user', (req, res) => {
+app.get("/api/user", (req, res) => {
   res.json([
     {
       id: 1,
-      name: '홍길동',
-      birthday: '961222',
-      gender: '200',
-      job: '대학생'
+      name: "홍길동",
+      birthday: "961222",
+      gender: "200",
+      job: "대학생",
     },
     {
       id: 2,
-      name: '김김김',
-      birthday: '931222',
-      gender: '200',
-      job: '프로그래머'
+      name: "김김김",
+      birthday: "931222",
+      gender: "200",
+      job: "프로그래머",
     },
     {
       id: 3,
-      name: '손손손',
-      birthday: '981222',
-      gender: '200',
-      job: '의사'
-    }
+      name: "손손손",
+      birthday: "981222",
+      gender: "200",
+      job: "의사",
+    },
   ]);
 });
 
