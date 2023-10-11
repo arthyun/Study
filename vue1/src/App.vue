a9ec386a1fb51d2f8d297b6126499248a2ca4d19
arthyun <heun3316@naver.com>
2023-10-11 19:12:49 +0900

    vue 입문3

--- vue1/src/App.vue
+++ vue1/src/App.vue
@@ -1,10 +1,12 @@
 <template>
-  <img
-    alt="Vue logo"
-    src="./assets/logo.png"
-  />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
-  <button @click="printEnv">axios 호출</button>
+  <div>
+    <img
+      alt="Vue logo"
+      src="./assets/logo.png"
+    />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <button @click="printEnv">axios 호출</button>
+  </div>
 </template>
 
 <script>
--- vue1/src/components/HelloWorld.vue
+++ vue1/src/components/HelloWorld.vue
@@ -3,39 +3,62 @@
     <h1>{{ msg }}</h1>
 
     <button @click="toggleCount">boolean 변경</button>
-    <button v-if="count1" @click="control1">현재 숫자: {{ count2 }}</button>
-    <button v-else @click="control2">현재 숫자: {{ count2 }}</button>
-    <br/>
-    <br/>
-    <input type="text" class="textZone" />
+    <button
+      v-if="count1 === true"
+      @click="control1"
+    >
+      현재 숫자: {{ count2 }}
+    </button>
+    <button
+      v-else
+      @click="control2"
+    >
+      현재 숫자: {{ count2 }}
+    </button>
+    <br />
+    <br />
+    <input
+      type="text"
+      class="textZone"
+    />
     <button @click="arrHandle2">arr2에 값 추가</button>
     <ul>
-      <li v-for="(list, i) in arr2" v-bind:key="i">
-        <p style="display: inline-block; margin-right: 10px;">{{ list.id }}</p>
-        <p style="display: inline-block;">{{ list.name }}</p>
+      <li
+        v-for="(list, i) in arr2"
+        v-bind:key="i"
+      >
+        <p style="display: inline-block; margin-right: 10px">{{ list.id }}</p>
+        <p style="display: inline-block">{{ list.name }}</p>
       </li>
     </ul>
 
-
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
+      For a guide and recipes on how to configure / customize this project,<br />
       check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+      <a
+        href="https://cli.vuejs.org"
+        target="_blank"
+        rel="noopener"
+        >vue-cli documentation</a
+      >.
     </p>
 
-    <h3 v-for="(data, i) in arr" :key="data">
-    {{ data.post1 + ` / index ${i}` }}
+    <h3
+      v-for="(data, i) in arr"
+      :key="data"
+    >
+      {{ data.post1 + ` / index ${i}` }}
     </h3>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String
+    msg: String,
   },
-  data(){
+  data() {
     return {
       //v-if, v-else, v-for - used;
       count1: false,
@@ -43,41 +66,41 @@ export default {
       arr: [
         {
           id: 1,
-          post1: "Installed CLI Plugins1"
+          post1: "Installed CLI Plugins1",
         },
         {
           id: 2,
-          post1: "Installed CLI Plugins2"
+          post1: "Installed CLI Plugins2",
         },
         {
           id: 3,
-          post1: "Installed CLI Plugins3"
+          post1: "Installed CLI Plugins3",
         },
       ],
-      arr2: []
-    }
+      arr2: [],
+    };
   },
   methods: {
-    control1(){
+    control1() {
       this.count2++;
     },
-    control2(){
+    control2() {
       this.count2--;
     },
-    toggleCount(){
+    toggleCount() {
       this.count1 = !this.count1;
     },
-    arrHandle2(){
+    arrHandle2() {
       const arrMain = this.arr2;
-      arrMain.push({ 
+      arrMain.push({
         id: Math.floor(Math.random() * 11),
-        name: document.querySelector('.textZone').value
+        name: document.querySelector(".textZone").value,
       });
       console.log(arrMain);
       console.log(this.arr2);
-    }
-  }
-}
+    },
+  },
+};
 // const dic = document.querySelector('.textZone');
 // console.log(dic.value)
 </script>
