312b5acdff20d082b5bd44f6200cc3075d33c882
arthyun <heun3316@naver.com>
2023-10-16 22:25:06 +0900

    next tsc 연습5.5

--- next4/app/page.tsx
+++ next4/app/page.tsx
@@ -1,9 +1,32 @@
 import React from "react";
 
+interface Result {
+   ID: number;
+   CONTENT: string;
+   CREATE_AT: string;
+   UPDATE_AT: string;
+}
+
 export default async function Home() {
+   const getMemos = await fetch("http://localhost:3000/api/getMemos");
+   const result = await getMemos.json();
+   console.log(result);
+
    return (
-      <>
+      <div>
          <h3>HelloWorld</h3>
-      </>
+         {result?.map((item: Result) => {
+            return (
+               <ul
+                  key={item.ID}
+                  style={{ margin: "17.5px" }}
+               >
+                  <li>{item.CONTENT}</li>
+                  <li style={{ fontSize: "12px" }}>{item.CREATE_AT}</li>
+                  <li style={{ fontSize: "12px" }}>{item.UPDATE_AT}</li>
+               </ul>
+            );
+         })}
+      </div>
    );
 }
