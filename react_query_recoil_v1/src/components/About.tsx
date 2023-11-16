584359b4a44415e10b78250ccc339748bfaf0a09
arthyun <heun3316@naver.com>
2023-11-16 19:54:33 +0900

    study recoil selector

--- react_query_recoil_v1/src/components/About.tsx
+++ react_query_recoil_v1/src/components/About.tsx
@@ -1,29 +1,29 @@
-import React, { useState } from 'react';
-import { countState, countStateSelector, selectedCountState } from '../store';
-import { useRecoilValue } from 'recoil';
+import React from "react";
+import { useRecoilState } from "recoil";
+import { userStoreAtom, userStoreSelector } from "../store";
 
 const About = () => {
-    const selectedCount = useRecoilValue<number>(countState);
-    const selectedSelector = useRecoilValue<string>(countStateSelector);
-    const [test1, setTest1] = useState<string>('감지되지 않음');
-    const [test2, setTest2] = useState<number>(0);
-    const selectedSelector2 = useRecoilValue<number>(selectedCountState(selectedCount));
+   const [userStoreValue, setUserStoreValue] = useRecoilState<number>(userStoreAtom);
+   const [storeSelector, setStoreSelector] = useRecoilState<number>(userStoreSelector);
 
-    return (
-        <div className='aboutWrap'>
-            <h3>초기 상태 값 : {selectedCount}</h3>
-
-            <button type='button' onClick={() => {
-                setTest1(selectedSelector);
-            }}>Selector 실행</button>
-            <h3>변경된 Selector 값 : {test1}</h3>
-
-            <button type='button' onClick={() => {
-                setTest2(selectedSelector2);
-            }}>SelectorFamily 실행</button>
-            <h3>변경된 SelectFamily 값 : {test2}</h3>
-        </div>
-    )
-}
+   return (
+      <div className="aboutWrap">
+         <h1>초기값 : {userStoreValue}</h1>
+         <button
+            type="button"
+            onClick={() => setUserStoreValue(userStoreValue + 1)}
+         >
+            초기값 변경하기
+         </button>
+         <h1>셀렉트 : {storeSelector}</h1>
+         <button
+            type="button"
+            onClick={() => setStoreSelector((prev) => prev)}
+         >
+            셀렉트 변경하기
+         </button>
+      </div>
+   );
+};
 
 export default About;
--- react_query_recoil_v1/src/store.tsx
+++ react_query_recoil_v1/src/store.tsx
@@ -1,22 +1,25 @@
-import { atom, selector, selectorFamily } from 'recoil';
+import { atom, selector, selectorFamily } from "recoil";
 
 // 컴포넌트에서 아톰을 사용하기 위해 export를 사용해 내보냅니다.
 export const countState = atom({
-  key: 'count',
-  default: 0
+   key: "count",
+   default: 0 as number,
 });
 export const nameState = atom({
-  key: 'name',
-  default: 'hyunho'
+   key: "name",
+   default: "hyunho",
 });
 
 //selector를 활용하여 get과 set 가능
 export const countStateSelector = selector({
-  key: 'countSelector1',
-  get: ({ get }) => {
-    const count = get(countState) + 'px';
-    return count;
-  }
+   key: "countSelector1",
+   get: ({ get }) => {
+      const count = get(countState);
+      return count;
+   },
+   //  set: ({ set }, newValue) => {
+   //     return countState + newValue;
+   //  },
 });
 // export const countWithUnitState = selector({
 //   key: 'countSelector2',
@@ -31,53 +34,74 @@ export const countStateSelector = selector({
 
 //매개변수를 주고자 할 때
 export const selectedCountState = selectorFamily({
-  key: 'countFamily',
-  get:
-    (num: number) =>
-    ({ get }) => {
-      const countInitial = get(countState);
-      return countInitial + num;
-    }
+   key: "countFamily",
+   get:
+      (num: number) =>
+      ({ get }) => {
+         const countInitial = get(countState);
+         return countInitial + num;
+      },
 });
 
 //배열 처리 테스트
 //타입 지정
 interface GetList {
-  id: number;
-  name: string;
-  username: string;
-  email: string;
-  address: {
-    street: string;
-    suite: string;
-    city: string;
-    zipcode: string;
-    geo: {
-      lat: string;
-      lng: string;
-    };
-  };
-  phone: string;
-  website: string;
-  company: {
-    name: string;
-    catchPhrase: string;
-    bs: string;
-  };
+   id: number;
+   name: string;
+   username: string;
+   email: string;
+   address: {
+      street: string;
+      suite: string;
+      city: string;
+      zipcode: string;
+      geo: {
+         lat: string;
+         lng: string;
+      };
+   };
+   phone: string;
+   website: string;
+   company: {
+      name: string;
+      catchPhrase: string;
+      bs: string;
+   };
 }
 export const getList = atom<GetList[]>({
-  key: 'getList',
-  default: []
+   key: "getList",
+   default: [],
 });
 
 //getList 변형
 export const getChangeList = selector({
-  key: 'getChangeList',
-  get: ({ get }) => {
-    const initialList = get(getList);
-    return initialList;
-  },
-  set: ({ set }, newValue) => {
-    set(getList, newValue);
-  }
+   key: "getChangeList",
+   get: ({ get }) => {
+      const initialList = get(getList);
+      return initialList;
+   },
+   set: ({ set }, newValue) => {
+      set(getList, newValue);
+   },
+});
+
+// Test
+export const userStoreAtom = atom({
+   key: "userStoreAtom",
+   default: 0,
+});
+
+export const userStoreSelector = selector({
+   key: "userStoreSelector",
+   get:
+      // (num: number) =>
+      ({ get }) => {
+         const getItem = get(userStoreAtom);
+         return getItem;
+      },
+   set: ({ set }, newValue) => {
+      // console.log(newValue);
+      const changeData = Number(newValue) / 1;
+      set(userStoreAtom, changeData);
+   },
 });
