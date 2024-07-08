1087500f596375694a59e6d674d3034e16f5dc4c
arthyun <heun3316@naver.com>
2024-07-09 01:40:50 +0900

    add zoom project

---
+++ zoom/src/server.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
+app.use('/public', express.static(__dirname + '/public'));
+app.get('/', (req, res) => res.render('home'));
+
+app.listen(3000, () => console.log('Listening on PORT 3000...'));
