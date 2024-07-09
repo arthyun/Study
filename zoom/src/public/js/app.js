13661e4262d5a589f1b9c8e09ba9cbfa95d2f259
arthyun <heun3316@naver.com>
2024-07-09 16:36:34 +0900

    modify zoom project v02

--- zoom/src/public/js/app.js
+++ zoom/src/public/js/app.js
@@ -1,33 +1,54 @@
 // 선택자
 const messageList = document.querySelector('ul');
-const messageForm = document.querySelector('form');
+const nickForm = document.querySelector('#nick');
+const messageForm = document.querySelector('#message');
 
-// WebSocket 연결
+// WebSocket Logic
+// 연결문
 const socket = new WebSocket(`ws://${window.location.host}`);
 
-// WebSocket Events
-// 연결 확인
+// 최초 연결 확인
 socket.addEventListener('open', () => {
   console.log('Open WebSocket Connection ⭕️');
 });
 
-// 메세지 받기
+// 서버로부터 메세지 받기
 socket.addEventListener('message', (message) => {
   const { data } = message;
-  console.log('New Message:', data);
+  // console.log('New Message:', data);
+  // 메세지를 전달 받을때마다 리스트로 보여주기
+  const li = document.createElement('li');
+  li.innerText = data;
+  messageList.append(li);
 });
 
-// 연결 해제
+// 연결 해제 (웹소켓 서버가 다운됐을때)
 socket.addEventListener('close', () => {
   console.log('Close WebSocket Connection ❌');
 });
 
-// 서버로 메세지 전달
+// 보낼 메세지를 JSON.stringify로 변환 함수
+const mageMessage = (type, payload) => {
+  const msg = { type, payload };
+  return JSON.stringify(msg);
+};
+
+// 서버로 메세지 전달 (nickname/message)
+// 닉네임 입력란
+const handleNickSubmit = (e) => {
+  e.preventDefault();
+  const input = nickForm.querySelector('input');
+  socket.send(mageMessage('nickname', input.value));
+  input.value = ''; // 제출 후 입력란 초기화
+};
+nickForm.addEventListener('submit', handleNickSubmit);
+
+// 메세지 입력란
 const handleSubmit = (e) => {
   e.preventDefault();
   const input = messageForm.querySelector('input');
   //   console.log(input.value);
-  socket.send(input.value);
-  input.value = '';
+  socket.send(mageMessage('message', input.value));
+  input.value = ''; // 제출 후 입력란 초기화
 };
 messageForm.addEventListener('submit', handleSubmit);
--- zoom/src/server.js
+++ zoom/src/server.js
@@ -24,22 +24,22 @@ wss.on('connection', (socket) => {
   // 페이지 접속시 접속자 확인을 위해 접속자 순서대로 배열에 추가함
   sockets.push(socket);
 
-  // 수신 이벤트 관련
+  // 수신 이벤트 관련 (브라우저에서 메세지 전달받을때)
   socket.on('message', (message, isBinary) => {
-    // console.log(message);
-    // socket.send(message);
+    // { type: '', payload: '' } 형태로 받아서 분기처리 예정
     message = isBinary ? message : message.toString(); // 메세지가 바이너리가 아닐때 변환해서 보내주어야함
-    // 접속중인 모든 사용자가 메세지를 보낼 수 있게함
-    sockets.forEach((aSocket) => {
-      aSocket.send(message);
+    // 접속중인 모든 사용자가 메세지를 보낼 수 있게함 (다시 브라우저로 메세지 전달)
+    sockets.forEach((socketChildren) => {
+      // 사용자 구분을 위해 json형태로 보내자
+      socketChildren.send(message);
     });
   });
   socket.on('close', () => {
     console.log('Disconnected from the Browser ❌');
   });
 
-  // 연결시 브라우저로 보내는 초기 값
-  socket.send('Welcome to WebSocket Server!!!');
+  // // 연결시 브라우저로 보내는 초기 값
+  // socket.send('Welcome to WebSocket Server!!!');
 });
 
 // 서버 연결 확인
